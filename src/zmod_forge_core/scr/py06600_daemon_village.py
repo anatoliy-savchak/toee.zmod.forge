420998743c6d3b341bcea306ff1ad50aa6e79b19
anatoliy-savchak <40269424+anatoliy-savchak@users.noreply.github.com>
2022-10-14 14:46:36 +0300

    Kerowyn Hucrele

--- src/zmod_forge_core/scr/py06600_daemon_village.py
+++ src/zmod_forge_core/scr/py06600_daemon_village.py
@@ -1,6 +1,7 @@
 import toee, debug, ctrl_daemon, ctrl_daemon2, utils_toee, utils_storage, utils_obj, utils_item, const_proto_weapon, const_proto_armor, const_toee
 import ctrl_behaviour, py06122_cormyr_prompter, factions_zmod, const_proto_scrolls, const_proto_wands, utils_npc
-import module_consts, const_proto_sceneries, utils_locks, py14710_smith, py14711_smith_wife, py14712_wizard, py14713_priest, utils_toee, py06601_village_npc
+import module_consts, const_proto_sceneries, utils_locks, utils_toee
+import py14710_smith, py14711_smith_wife, py14712_wizard, py14713_priest, py06601_village_populace, py06602_village_npc
 
 VILLAGE_DAEMON_SCRIPT_ID = 6600
 VILLAGE_DAEMON_ID = "G_777CFFCC_1864_4C4D_B05F_ECE4C6EF4B18"
@@ -36,13 +37,10 @@ class CtrlVillage(ctrl_daemon2.CtrlDaemon2):
 		return
 
 	def place_encounters_initial(self):
-		self.create_npc_at(utils_obj.sec2loc(478, 508), py14710_smith.CtrlVillageSmith, const_toee.ROT09, "merchant", "smith", None, 0, 1)
-		self.create_npc_at(utils_obj.sec2loc(476, 505), py14711_smith_wife.CtrlVillageSmithWife, const_toee.ROT08, "merchant", "smith_wife", None, 0, 1)
-		self.create_npc_at(utils_obj.sec2loc(503, 477), py14712_wizard.CtrlVillageWizard, const_toee.ROT02, "merchant", "wizard", None, 0, 1)
-		self.create_npc_at(utils_obj.sec2loc(494, 506), py14713_priest.CtrlVillagePriest, const_toee.ROT11, "merchant", "priest", None, 0, 1)
-
 		#self.generate_animals()
-		self.generate_wanderers()
+		#self.generate_wanderers()
+		self.place_merchants()
+		self.place_tavern()
 
 		return
 
@@ -53,7 +51,7 @@ class CtrlVillage(ctrl_daemon2.CtrlDaemon2):
 	def generate_animals(self):
 		# there are problems, skip for now
 		delay_base = 1*1000
-		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(474, 456), py06601_village_npc.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig1", None, 0, 1)
+		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(474, 456), py06601_village_populace.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig1", None, 0, 1)
 		if (npc):
 			if (1):
 				waypoints = list()
@@ -69,7 +67,7 @@ class CtrlVillage(ctrl_daemon2.CtrlDaemon2):
 				npc.npc_flag_set(toee.ONF_WANDERS)
 				npc.move(utils_obj.sec2loc(479, 461))
 				
-		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(475, 456), py06601_village_npc.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig2", None, 0, 1)
+		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(475, 456), py06601_village_populace.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig2", None, 0, 1)
 		if (npc):
 			if (1):
 				waypoints = list()
@@ -84,7 +82,7 @@ class CtrlVillage(ctrl_daemon2.CtrlDaemon2):
 			else:
 				npc.npc_flag_set(toee.ONF_WANDERS)
 
-		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(476, 456), py06601_village_npc.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig3", None, 0, 1)
+		npc, ctrl = self.create_npc_at(utils_obj.sec2loc(476, 456), py06601_village_populace.CtrlVillageAnimalPig, const_toee.rotation_1100_oclock, "animals", "pig3", None, 0, 1)
 		if (npc):
 			if (1):
 				waypoints = list()
@@ -102,7 +100,18 @@ class CtrlVillage(ctrl_daemon2.CtrlDaemon2):
 
 	def generate_wanderers(self):
 		for num in range(1, 10):
-			x, y = py06601_village_npc.VillagePlaces.get_random_sqare_place()
-			npc, ctrl = self.create_npc_at(utils_obj.sec2loc(x, y), py06601_village_npc.CtrlVillageRandomWanderer, const_toee.rotation_1100_oclock, "wanderers", "person{}".format(num), None, 0, 1)
+			x, y = py06601_village_populace.VillagePlaces.get_random_sqare_place()
+			npc, ctrl = self.create_npc_at(utils_obj.sec2loc(x, y), py06601_village_populace.CtrlVillageRandomWanderer, const_toee.ROT02, "wanderers", "person{}".format(num), None, 0, 1)
 			ctrl.make_day_route(npc)
+		return
+
+	def place_tavern(self):
+		self.create_npc_at(utils_obj.sec2loc(492, 461), py06602_village_npc.CtrlKerowyn, const_toee.ROT02, "main", "kerowyn", None, 0, 1)
+		return
+
+	def place_merchants(self):
+		self.create_npc_at(utils_obj.sec2loc(478, 508), py14710_smith.CtrlVillageSmith, const_toee.ROT09, "merchant", "smith", None, 0, 1)
+		self.create_npc_at(utils_obj.sec2loc(476, 505), py14711_smith_wife.CtrlVillageSmithWife, const_toee.ROT08, "merchant", "smith_wife", None, 0, 1)
+		self.create_npc_at(utils_obj.sec2loc(503, 477), py14712_wizard.CtrlVillageWizard, const_toee.ROT02, "merchant", "wizard", None, 0, 1)
+		self.create_npc_at(utils_obj.sec2loc(494, 506), py14713_priest.CtrlVillagePriest, const_toee.ROT11, "merchant", "priest", None, 0, 1)
 		return
---
+++ src/zmod_forge_core/scr/py06602_village_npc.py
@@ -0,0 +1,68 @@
+import toee, ctrl_behaviour, utils_item, utils_obj, const_toee, factions_zmod, utils_npc, const_animate
+import const_proto_armor, const_proto_weapon, const_proto_food, const_proto_cloth, const_proto_containers, const_proto_list_weapons, const_proto_list_scrolls, const_proto_list_cloth
+import random, debug
+import const_proto_npc, module_quests
+
+MODULE_SCRIPT_ID = 6602
+
+def san_start_combat(attachee, triggerer): return ctrl_behaviour.san_start_combat(attachee, triggerer)
+def san_enter_combat(attachee, triggerer): return ctrl_behaviour.san_enter_combat(attachee, triggerer)
+def san_end_combat(attachee, triggerer): return ctrl_behaviour.san_end_combat(attachee, triggerer)
+def san_exit_combat(attachee, triggerer): return ctrl_behaviour.san_exit_combat(attachee, triggerer)
+def san_will_kos(attachee, triggerer): return ctrl_behaviour.san_will_kos(attachee, triggerer)
+def san_dialog(attachee, triggerer): return ctrl_behaviour.san_dialog(attachee, triggerer)
+def san_heartbeat(attachee, triggerer): return ctrl_behaviour.san_heartbeat(attachee, triggerer)
+def san_wield_off(attachee, triggerer): return ctrl_behaviour.san_wield_off(attachee, triggerer)
+
+class CtrlKerowyn(ctrl_behaviour.CtrlBehaviourAI):
+	@classmethod
+	def get_proto_id(cls): return const_proto_npc.PROTO_NPC_WOMAN
+
+	def after_created(self, npc):
+		assert isinstance(npc, toee.PyObjHandle)
+		npc.scripts[const_toee.sn_dialog] = MODULE_SCRIPT_ID
+
+		utils_npc.npc_description_set_new(npc, "Kerowyn Hucrele")
+
+		npc.make_class(const_toee.stat_level_npc_expert, 5)
+
+		npc.obj_set_int(toee.obj_f_critter_portrait, 7370) # Matron_Mathilde_14043
+		hairStyle = utils_npc.HairStyle.from_npc(npc)
+		hairStyle.style = const_toee.hair_style_medium
+		hairStyle.color = const_toee.hair_color_black
+		hairStyle.update_npc(npc)
+
+		npc.object_flag_set(toee.OF_INVULNERABLE)
+		npc.npc_flag_set(toee.ONF_NO_ATTACK)
+
+		# create inventory
+		utils_item.item_create_in_inventory(self.get_cloth_garb(), npc, 1, 1)
+		utils_item.item_create_in_inventory2(const_proto_cloth.PROTO_CLOTH_CIRCLET_HOODLESS, npc, 1, toee.item_wear_helmet)
+		utils_item.item_create_in_inventory(const_proto_cloth.PROTO_CLOTH_BOOTS_LEATHER_BOOTS_BLACK, npc, 1, 1)
+		utils_item.item_money_create_in_inventory(npc, 0, 500)
+		
+		npc.item_wield_best_all()
+		utils_npc.npc_generate_hp_avg_first(npc)
+		return
+
+	def get_cloth_garb(self): return const_proto_cloth.PROTO_CLOTH_ROBES_GREEN
+
+	def dialog(self, attachee, triggerer):
+		assert isinstance(attachee, toee.PyObjHandle)
+		assert isinstance(triggerer, toee.PyObjHandle)
+
+		attachee.turn_towards(triggerer)
+		if (not attachee.has_met(triggerer)):
+			triggerer.begin_dialog(attachee, 1) # meet and quest
+		else: 
+			triggerer.begin_dialog(attachee, 50) # after quest given
+		return toee.SKIP_DEFAULT
+
+	@classmethod
+	def dialog_give_quest(cls, npc, willing):
+		assert isinstance(npc, toee.PyObjHandle)
+		if willing:
+			toee.game.quests[module_quests.QUEST_VILLAGE_HUCRELE].state = toee.qs_accepted
+		else: 
+			toee.game.quests[module_quests.QUEST_VILLAGE_HUCRELE].state = toee.qs_mentioned
+		return
